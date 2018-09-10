49efa871ca58e0e87c6aa60f0c694d3ea160b4e0
fredericGette <frederic.gette@gmail.com>
2018-09-10 06:54:01 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const {google} = require('googleapis');
+const authorization = require('./authorization');
+const messages = require('./messages');
+
+authorization.loadCredentials(listMessages);
+
+/**
+ * Retrieve Messages in user's mailbox matching query.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listMessages(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  const query = 'from:(assistance@linxo.com) subject:notification';
+
+  messages.getPageOfMessages(gmail, query, []);
+};
