ff74167dde0b4ea3ef67990dfd24ac372360917d
Frédéric GETTE <frederic.gette@gmail.com>
2019-11-11 14:30:05 +0100

    FTP to store data

--- persister.js
+++ persister.js
@@ -1,6 +1,9 @@
 const { google } = require('googleapis');
 const SPREADSHEET_ID = '1h2EcgcjoNZN_F0lbg4zpTYQnE4xRtM26LP52JdS79hM';
 
+const FtpClient = require('ftp');
+const fs = require('fs');
+
 /**
  * Add information from the messages to the spreadsheet.
  * Information are added only when they don't alredy exist in the spreadsheet.
@@ -10,7 +13,7 @@ const SPREADSHEET_ID = '1h2EcgcjoNZN_F0lbg4zpTYQnE4xRtM26LP52JdS79hM';
  */
 exports.addTransactions = (sheets, messages) => {
 
-    filterNewMessages(sheets, messages,[]
+    filterNewMessages(sheets, messages, []
         , (newMessages) => {
             let rows = [];
             newMessages.forEach(message => {
@@ -18,16 +21,16 @@ exports.addTransactions = (sheets, messages) => {
                     account.transactions.forEach(transaction => {
                         // format date ISO 8601
                         let transactionDate = transaction.date.format('YYYY-MM-DD');
-    
+
                         let row = [message.id, transactionDate, account.name, transaction.type, transaction.label, transaction.category, transaction.amount, transaction.category2];
                         rows.push(row)
-                    });    
+                    });
                     let balanceDate = message.date.format('YYYY-MM-DD');
                     let row = [message.id, balanceDate, account.name, 'Balance', '', '', account.balance];
                     rows.push(row)
                 });
             });
-        
+
             sheets.spreadsheets.values.append({
                 spreadsheetId: SPREADSHEET_ID,
                 range: 'transactions',
@@ -37,10 +40,12 @@ exports.addTransactions = (sheets, messages) => {
                 },
             }, (err, result) => {
                 if (err) return console.log(err);
-        
+
                 console.log(`${result.data.updates.updatedCells} cells appended.`);
-        
-            });    
+
+                getAndStoreSheetData(sheets);
+
+            });
         });
 }
 
@@ -58,13 +63,13 @@ filterNewMessages = (sheets, inputMessages, outputMessages, callback) => {
         includeValuesInResponse: true,
         resource: {
             values: [
-                ['=NOT(ISNA(VLOOKUP("'+message.id+'", transactions!A:A, 1, false)))']
+                ['=NOT(ISNA(VLOOKUP("' + message.id + '", transactions!A:A, 1, false)))']
             ]
         }
     }, (err, result) => {
         if (err) return console.log(err);
- 
-        console.log(`Check message [${inputMessages.length+1}]`);
+
+        console.log(`Check message [${inputMessages.length + 1}]`);
 
         let alreadyProcessed = result.data.updatedData.values[0][0];
         if (alreadyProcessed === 'FALSE') {
@@ -76,5 +81,49 @@ filterNewMessages = (sheets, inputMessages, outputMessages, callback) => {
         } else {
             callback(outputMessages);
         }
+    });
+}
+
+/**
+ * Get data from the sheet and store them to a file using FTP.
+ * 
+ * @param {goole.sheets} sheets
+ */
+getAndStoreSheetData = (sheets) => {
+    sheets.spreadsheets.values.batchGet({
+        spreadsheetId: SPREADSHEET_ID,
+        ranges: ['inouts!R2', 'balance!O4'],
+        majorDimension: 'ROWS',
+        valueRenderOption: 'UNFORMATTED_VALUE'
+    }, (err, result) => {
+        if (err) return console.log(err);
+
+        let deltaLast30days = result.data.valueRanges[0].values[0][0];
+        let currentBalance = result.data.valueRanges[1].values[0][0];
+
+        let data = {
+            currentBalance: currentBalance,
+            deltaLast30days: deltaLast30days
+        }
+        console.log(`
+            Current balance:    ${currentBalance}
+            Delta last 30 days: ${deltaLast30days}
+        `);
+
+        var c = new FtpClient();
+        c.on('ready', function () {
+            console.log("FTP: put");
+            c.put(JSON.stringify(data), 'Bankroute/bankroute.json', function (err) {
+                if (err) throw err;
+                c.end();
+                console.log("FTP: end");
+            });
+        });
+        console.log("FTP: connect");
+        c.connect({
+            host: 'perso-ftp.orange.fr',
+            user: 'fredericgette@orange.fr',
+            password: 'Orange25'
+        });
     });
 }
